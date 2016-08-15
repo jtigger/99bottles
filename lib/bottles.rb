79cf425fc27af17150e479a90f2c82e889cd2861
John S. Ryan <jtigger@infosysengr.com>
2016-08-14 20:51:39 -0700

    Extract Bottle :: container that fits one drink, per.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,36 +1,52 @@
 class Bottles
   def verse(drinks)
+    container = Bottle.new(drinks)
     case drinks
     when 0
-      "#{quantity(drinks).capitalize} #{name(drinks)} of beer on the wall, #{quantity(drinks)} #{name(drinks)} of beer.\n" +
-      "Go to the store and buy some more, 99 #{name(drinks-1)} of beer on the wall.\n"
+      "#{container.quantity.capitalize} #{container.name} of beer on the wall, #{container.quantity} #{container.name} of beer.\n" +
+      "Go to the store and buy some more, #{container.one_fewer.quantity} #{container.one_fewer.name} of beer on the wall.\n"
     when 1
-      "#{quantity(drinks)} #{name(drinks)} of beer on the wall, #{quantity(drinks)} #{name(drinks)} of beer.\n" +
-      "Take it down and pass it around, #{quantity(drinks-1)} #{name(drinks-1)} of beer on the wall.\n"
+      "#{container.quantity.capitalize} #{container.name} of beer on the wall, #{container.quantity} #{container.name} of beer.\n" +
+      "Take it down and pass it around, #{container.one_fewer.quantity} #{container.one_fewer.name} of beer on the wall.\n"
     when 2
-      "#{quantity(drinks)} #{name(drinks)} of beer on the wall, #{quantity(drinks)} #{name(drinks)} of beer.\n" +
-      "Take one down and pass it around, #{quantity(drinks-1)} #{name(drinks-1)} of beer on the wall.\n"
+      "#{container.quantity.capitalize} #{container.name} of beer on the wall, #{container.quantity} #{container.name} of beer.\n" +
+      "Take one down and pass it around, #{container.one_fewer.quantity} #{container.one_fewer.name} of beer on the wall.\n"
     else
-      "#{quantity(drinks)} #{name(drinks)} of beer on the wall, #{quantity(drinks)} #{name(drinks)} of beer.\n" +
-      "Take one down and pass it around, #{quantity(drinks-1)} #{name(drinks-1)} of beer on the wall.\n"
+      "#{container.quantity.capitalize} #{container.name} of beer on the wall, #{container.quantity} #{container.name} of beer.\n" +
+      "Take one down and pass it around, #{container.one_fewer.quantity} #{container.one_fewer.name} of beer on the wall.\n"
     end
   end
 
-  def quantity(drinks)
-    if drinks == 0
-      "no more"
-    else
-      drinks.to_s
+  class Bottle
+    def initialize(drinks)
+      @drinks = drinks
+    end
+
+    def quantity
+      if @drinks == 0
+        "no more"
+      else
+        @drinks.to_s
+      end
+    end
+
+    def name
+      if @drinks == 1
+        "bottle"
+      else
+        "bottles"
+      end
+    end
+
+    def one_fewer
+      if @drinks == 0
+        Bottle.new(99)
+      else
+        Bottle.new(@drinks - 1)
+      end
     end
   end
 
-  def name(drinks)
-    if drinks == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
 
   def verses(drinks_to_start, drinks_to_end)
     drinks_to_start
